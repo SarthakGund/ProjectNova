b56cc6daebc2dcff37ccf04c6e1ee5c384f1e2fe
SarthakGund <sarthakdg03@gmail.com>
2024-02-17 08:11:35 +0530

    Edited with form

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route ('')
+
+def convert():
+    order1 = request.form['order1']
